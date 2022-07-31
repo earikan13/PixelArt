f47d5ff20bb327010981df726d50f4ef8bd0ecf4
earikan13 <arikanefe96@gmail.com>
2022-07-31 19:06:22 +0200

    creating and optimizing GIFs

--- PixelArt.py
+++ PixelArt.py
@@ -9,6 +9,9 @@ Created on Sat Jul 30 21:10:47 2022
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
+import imageio.v3 as iio
+import time
+from pygifsicle import optimize
 
 
 class PixelArt:
@@ -18,7 +21,6 @@ class PixelArt:
         self.img = cv2.imread(self.imgName + '.' + str(imgExtension))
         self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
         self.imgHeight, self.imgWidth = self.img.shape[:-1]
-        print((self.imgHeight, self.imgWidth))
 
     def pixelImage(self):
         scale_percent = 3  # percent of original size
@@ -28,12 +30,20 @@ class PixelArt:
             self.img, (width, height), interpolation=cv2.INTER_LINEAR)
         self.pixelledImage = cv2.resize(
             self.pixelledImage, (self.imgWidth, self.imgHeight), interpolation=cv2.INTER_NEAREST)
-        print(self.pixelledImage.shape)
 
-    def combineImages(self):
-        tmpPixelImg = self.pixelledImage[:, :self.imgWidth//2]
-        tmpImg = self.img[:, self.imgWidth//2:]
+    def combineImages(self, step):
+        step = int(self.imgWidth * step / 100)
+        tmpPixelImg = self.pixelledImage[:, :step]
+        tmpImg = self.img[:, step:]
         self.combinedImg = np.hstack((tmpPixelImg, tmpImg))
+
+    def generateGIF(self, dur=5):
+        images = []
+        for i in np.arange(0, 100, 5):
+            self.combineImages(i)
+            images.append(self.combinedImg)
+        iio.imwrite('PixelledGIF.gif', images, duration=dur)
+        optimize('PixelledGIF.gif')
 
     def generateDirection(self):
         rng = np.random.default_rng()
@@ -47,3 +57,15 @@ class PixelArt:
         ax.axis("off")
         fig.savefig("Pixelled" + self.imgName + '.' + 'png',
                     bbox_inches='tight', pad_inches=0, dpi=300)
+
+
+def main():
+    start = time.process_time()
+    pA = PixelArt("exampleImg", "jpg")
+    pA.pixelImage()
+    # pA.plotImage()
+    pA.generateGIF(0.5)
+    print(time.process_time() - start)
+
+
+main()
