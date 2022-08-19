243c38b1f9d31027d382af33f8673f4f49232fed
efeari <arikanefe96@gmail.com>
2022-08-19 15:36:46 +0000

    Finally a stable version

--- show.py
+++ show.py
@@ -14,19 +14,21 @@ path = Path(currentDir)
 
 def create_piclist():
     pwd = os.getcwd()
-    os.system("ls -1v -1 " + pwd +"/*.png > " +pwd + "/piclist.txt")
+    os.system("ls -1v -1 " + pwd + "/*.png > " +pwd + "/piclist.txt")
 
-def kill_fbi():
-    os.system("sudo killall -15 fbi")
-    
+def kill_fbi(sleep, dur=2):
+    if not sleep:
+        os.system("sleep " + str(dur * 21))
+    os.system("kill $(pgrep fbi)")
 def animate(dur=2):
-    os.system("fbi -d /dev/fb0 -T 1 -t " + str(dur) + " -cachemem 0 --noverbose --list piclist.txt")
+    os.system("fbi -d /dev/fb0 -T 1 -t " + str(dur) + "--noonce --noverbose --list piclist.txt")
     pass
 
 def main():
+    kill_fbi(True)
     for filename in os.listdir(path):
-        tmp=os.chdir(Path(currentDir + '/'  + filename))
-        create_piclist()
-        animate()
-        kill_fbi()
-
+       if os.path.isdir(Path(currentDir + '/'  + filename)):
+           tmp=os.chdir(Path(currentDir + '/'  + filename))
+           create_piclist()
+           animate()
+           kill_fbi(False)
