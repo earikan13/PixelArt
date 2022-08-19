dbf56248e6cd9d67a53ee1da59c959c3569fca20
efeari <arikanefe96@gmail.com>
2022-08-19 13:50:14 +0200

    now using PIL and numpy instead of opencv

--- PixelArt.py
+++ PixelArt.py
@@ -7,10 +7,10 @@ Created on Sat Jul 30 21:10:47 2022
 """
 
 import matplotlib.pyplot as plt
-import cv2
 import numpy as np
 import os
 from pathlib import Path
+from PIL import Image, ImageEnhance
 
 class PixelArt:
 
@@ -18,48 +18,52 @@ class PixelArt:
         self.downscalePerc = downscalePerc
         self.imgName = str(img).rsplit('/', -1)[-1]
         self.path = os.path.dirname(os.path.dirname(img))
-        self.img = cv2.imread(img + '.' + str(imgExtension))
-        self.imgHeight, self.imgWidth = self.img.shape[:2]
+        self.img = Image.open(img + '.' + str(imgExtension))
+        self.imgWidth, self.imgHeight = self.img.size[:2]
         if self.imgHeight != 320 or self.imgWidth != 480:
             self.imgHeight = 320
             self.imgWidth = 480
-            self.img = cv2.resize(
-                self.img, (480, 320), cv2.INTER_LINEAR)
-            cv2.imwrite(img + '.' + imgExtension, self.img)
-        self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
-        self.img = cv2.bitwise_not(self.img)        
-        self.img = cv2.addWeighted(self.img, 0.5, self.img, 0, -35)
+            self.img = self.img.resize((self.imgWidth, self.imgHeight), Image.BILINEAR)
+            self.img.save(img + '.' + imgExtension)
+        self.img = self.img.convert('L')
+        self.img = Image.eval(self.img, lambda px: (255 - px))
+        #self.img = Image.blend(self.img, self.img, 0.4)
+        enhancer = ImageEnhance.Contrast(self.img)
+        self.img = enhancer.enhance(0.5)
+        self.img = Image.eval(self.img, lambda px: (px - 110))
 
     def pixelImage(self):
         scale_percent = 3 * self.downscalePerc  # percent of original size
-        width = int(self.img.shape[1] * scale_percent / 150)
-        height = int(self.img.shape[0] * scale_percent / 150)
-        self.pixelledImage = cv2.resize(
-            self.img, (width, height), interpolation=cv2.INTER_LINEAR)
-        self.pixelledImage = cv2.resize(
-            self.pixelledImage, (self.imgWidth, self.imgHeight), interpolation=cv2.INTER_NEAREST)
+        width = int(self.img.size[1] * scale_percent / 150)
+        height = int(self.img.size[0] * scale_percent / 150)
+        self.pixelledImage = self.img.resize(
+            (width, height), Image.BILINEAR)
+        self.pixelledImage = self.pixelledImage.resize(
+            (self.imgWidth, self.imgHeight), Image.NEAREST)
 
     def combineImages(self, step, direction):
+        _image_pix = np.asarray(self.pixelledImage)
+        _image = np.asarray(self.img)
         if direction == 0:  # Left to right
             step = int(self.imgWidth * step / 100)
-            tmpPixelImg = self.pixelledImage[:, :step]
-            tmpImg = self.img[:, step:]
+            tmpPixelImg = _image_pix[:, :step]
+            tmpImg = _image[:, step:]
             combinedImg = np.hstack((tmpPixelImg, tmpImg))
         elif direction == 1:  # Right to left
             step = int(self.imgWidth * step / 100)
-            tmpPixelImg = self.pixelledImage[:,
-                                             self.pixelledImage.shape[1]-step:]
-            tmpImg = self.img[:, :self.pixelledImage.shape[1]-step]
+            tmpPixelImg = _image_pix[:,
+                                             self.pixelledImage.size[0]-step:]
+            tmpImg = _image[:, :self.pixelledImage.size[0]-step]
             combinedImg = np.hstack((tmpImg, tmpPixelImg))
         elif direction == 2:  # Down to up
             step = int(self.imgHeight * step / 100)
-            tmpPixelImg = self.pixelledImage[self.pixelledImage.shape[0]-step:]
-            tmpImg = self.img[:self.pixelledImage.shape[0]-step]
+            tmpPixelImg = _image_pix[self.pixelledImage.size[1]-step:]
+            tmpImg = _image[:self.pixelledImage.size[1]-step]
             combinedImg = np.vstack((tmpImg, tmpPixelImg))
         elif direction == 3:  # Up to down
             step = int(self.imgHeight * step / 100)
-            tmpPixelImg = self.pixelledImage[:step]
-            tmpImg = self.img[step:]
+            tmpPixelImg = _image_pix[:step]
+            tmpImg = _image[step:]
             combinedImg = np.vstack((tmpPixelImg, tmpImg))
         return combinedImg
     
@@ -71,9 +75,9 @@ class PixelArt:
         cnt = 0
         for i in np.arange(0, 105, 5):
             img_name = str(directory) + '/' + self.imgName + '-' + str(cnt) + ".png"
-            cv2.imwrite(img_name, self.combineImages(i, direction))
+            img = self.combineImages(i, direction)
+            Image.fromarray(img).save(img_name)
             cnt += 1
-        
 
     def generateDirection(self):
         rng = np.random.default_rng()
